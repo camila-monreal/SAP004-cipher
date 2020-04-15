041c8c182ec1c26629fa1dce57456662dd1657b5
Camila Monreal <moncml@outlook.com>
2020-04-15 17:59:59 -0300

    hacker edition

--- src/cipher.js
+++ src/cipher.js
@@ -16,25 +16,40 @@ const cipher = {
     }
     return lettersMessage.join("");
   },
-  cipherOffSet: function cipherOffSet(charcode, offset){
-    var addOffSetCesar;
-    var messageAddOffSetCesar=[];
-    for (addOffSetCesar of charcode){
-      messageAddOffSetCesar.push(((addOffSetCesar - 65 + offset) % 26) +65);
-    }
-    return messageAddOffSetCesar;
-  },
-  decipherOffSet: function decipherOffSet(charcode, offset){
-    var subOffSetCesar;
-    var messageSubOffSetCesar=[];
-    for (subOffSetCesar of charcode){
-      if (((subOffSetCesar - 65 - offset) % 26) >= 0){
-        messageSubOffSetCesar.push(((subOffSetCesar - 65 - offset) % 26)+65);
-      }else{
-        messageSubOffSetCesar.push(((subOffSetCesar - 65 - offset) % 26) +91);
+  AddOffSet: function AddOffSet(charcode, offset){
+    let OffSetCesar;
+    let messageOffSet=[];
+    for(OffSetCesar of charcode){
+      console.log(OffSetCesar);
+      if((OffSetCesar >= 65) && (OffSetCesar <= 90)){
+          messageOffSet.push(((OffSetCesar - 65 + offset) % 26)+65);
+      }
+      else if((OffSetCesar >=97) && (OffSetCesar <=122)){
+        messageOffSet.push(((OffSetCesar - 97 + offset) % 26)+97);
+      }
+      else{
+        messageOffSet.push(OffSetCesar);
       }
     }
-    return messageSubOffSetCesar;
+    return messageOffSet;
+  },
+  subtractTheOffset: function subtractTheOffset(charcode, offset){
+    let subOffSetCesar;
+    let messageSubOffSet=[];
+    for(subOffSetCesar of charcode){
+      if((subOffSetCesar >= 65) && (subOffSetCesar <= 90)){
+        if(((subOffSetCesar - 65 - offset) % 26) >= 0){
+          messageSubOffSet.push(((subOffSetCesar - 65 - offset) % 26)+65);
+        }
+      }if((subOffSetCesar >=97) && (subOffSetCesar <=122)){
+        if(((subOffSetCesar - 97 - offset) % 26) >= 0){
+          messageSubOffSet.push(((subOffSetCesar - 97 - offset) % 26)+97);
+        }
+      }else{
+        messageSubOffSet.push(subOffSetCesar)
+      }
+    }
+    return messageSubOffSet;
   },
   encode: function encode(offset, text){
     if (typeof(offset)!= "number"){ 
@@ -44,11 +59,13 @@ const cipher = {
       throw new TypeError;
     } 
     var charcode = cipher.messageToCharcode(text);
-    var messageAddOffSetCesar = cipher.cipherOffSet(charcode,offset);
+    var messageAddOffSetCesar = cipher.AddOffSet(charcode,offset);
+    console.log(messageAddOffSetCesar);
     var lettersMessage = cipher.AddOffSetCharcodeToLettersMessage(messageAddOffSetCesar);
     return lettersMessage;
   },
   decode: function decode(offset,text){
+    console.log("caraleo")
     if (typeof(offset)!= "number"){ 
       throw new TypeError;
     }
@@ -56,9 +73,12 @@ const cipher = {
       throw new TypeError;
     }
     var charcode = cipher.messageToCharcode(text);
-    var messageAddOffSetCesar = cipher.decipherOffSet(charcode,offset);
-    var lettersMessage = cipher.AddOffSetCharcodeToLettersMessage(messageAddOffSetCesar);
-    return lettersMessage
+    console.log(charcode);
+    var messageSubOffSet = cipher.subtractTheOffset(charcode,offset);
+    console.log(messageSubOffSet);
+    var lettersMessage = cipher.AddOffSetCharcodeToLettersMessage(messageSubOffSet);
+    console.log(lettersMessage);
+    return lettersMessage;
   }
 }
 export default cipher;
